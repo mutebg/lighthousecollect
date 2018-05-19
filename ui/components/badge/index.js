434531776e0ff104598e0302187613025fc1dd1f
Stoyan Delev <s.delev@elements.nl>
2018-05-19 10:34:44 +0200

    last commit before holidays

--- ui/components/badge/index.js
+++ ui/components/badge/index.js
@@ -1,7 +1,7 @@
 import "./style";
 
 const reportStatus = value => {
-  if (value > 75) return "pass";
+  if (value > 74) return "pass";
   else if (value > 45) return "average";
   else return "fail";
 };
