03e2bdd2f745e3b347e25e02d05f982c56b31004
Stoyan Delev <s.delev@elements.nl>
2018-06-10 17:28:12 +0200

    Add goal in transformations

--- api/transforms.js
+++ api/transforms.js
@@ -1,8 +1,17 @@
-const overview = raw =>
-  raw.reportCategories.map(({ name, score }) => ({
-    label: name,
-    value: Math.round(score)
-  }));
+const overview = (raw, { categories }) => {
+  const categoriesSet = categories.reduce((prev, curr) => {
+    prev[curr.name] = curr.value;
+    return prev;
+  }, {});
+  return raw.reportCategories.map(({ name, score }) => {
+    const goalScore = categoriesSet[name];
+    return {
+      label: name,
+      value: Math.round(score),
+      goal: goalScore !== "undefined" ? goalScore : 0
+    };
+  });
+};
 
 module.exports = {
   overview
